7576a7c8a90aa3ecde03806ecaacdc36983f9a4c
ROpdam <robinopdam@hotmail.com>
2020-03-18 18:02:22 +0100

    Changes after first run and ranking

--- eval_rank.py
+++ eval_rank.py
@@ -6,22 +6,23 @@ def eval_rank(args=[]):
     users = args[1]
     items = args[2]
     test_ones = args[3]
+    rank_at = args[4]
 
-    pred = pd.DataFrame(index=users, columns=['ranked_items', 'score'])
-    real = pd.DataFrame(index=users, columns=['ranked_items', 'score'])
+    pred = pd.DataFrame(index=users, columns=['score', 'ranked_items'])
+    real = pd.DataFrame(index=users, columns=['score', 'ranked_items'])
 
     for u in users:
         user_item_pred_score = []
         user_item_true_value = []
         for i in items:
-            user_item_pred_score.append((i, np.dot(result['p'][u], result['q'][i])))  # tuple list
-            user_item_true_value.append((i, test_ones[u, i]))
+            user_item_pred_score.append((np.dot(result['p'][u], result['q'][i]), i))  # tuple list
+            user_item_true_value.append((test_ones[u, i], i))
 
         user_item_pred_score.sort(reverse=True)
         user_item_true_value.sort(reverse=True)
 
-        pred_item_ids, pred_item_scores = zip(*user_item_pred_score)
-        real_item_ids, real_item_scores = zip(*user_item_true_value)
+        pred_item_ids, pred_item_scores = zip(*user_item_pred_score[:rank_at])
+        real_item_ids, real_item_scores = zip(*user_item_true_value[:rank_at])
 
         pred.loc[u]['ranked_items'] = pred_item_ids
         pred.loc[u]['score'] = pred_item_scores
