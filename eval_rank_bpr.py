9d6d22f063dee0826bcddac9894da19d062a063f
ROpdam <robinopdam@hotmail.com>
2020-04-08 20:02:07 +0200

    BPR: Ready, CFRNN: Finetune Evaluation

--- eval_rank_bpr.py
+++ eval_rank_bpr.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 
 def eval_rank_bpr(args=[]):
     model = args[0]
@@ -9,7 +8,7 @@ def eval_rank_bpr(args=[]):
     test_user_items = args[3]
     rank_at = args[4]
 
-    result_df = pd.DataFrame(index=users, columns=['scores_ranked', 'pred_items_ranked', 'true_id'])
+    result_df = pd.DataFrame(index=users, columns=['pred_items_ranked', 'true_id'])
 
     for u in users:
         user_item_pred_score = []
@@ -17,15 +16,12 @@ def eval_rank_bpr(args=[]):
         for true_item in test_user_items.loc[u]:
             true_items.append(true_item)
             
-        for i in items:
-            prediction = np.dot(model['p'][u], model['q'][i].T)
-            user_item_pred_score.append((prediction, i))  # tuple list
+        predictions = np.dot(model['p'][u], model['q'].T)
+        ids = np.argpartition(predictions, -rank_at)[-rank_at:]
+        best_ids = np.argsort(predictions[ids])[::-1]
+        best = ids[best_ids]
 
-        user_item_pred_score.sort(reverse=True) #np.argpartition
-        pred_item_scores, pred_item_ids = zip(*user_item_pred_score[:rank_at])
-
-        result_df.loc[u]['scores_ranked'] = pred_item_scores
-        result_df.loc[u]['pred_items_ranked'] = pred_item_ids
+        result_df.loc[u]['pred_items_ranked'] = best
         result_df.loc[u]['true_id'] = true_items
 
     return result_df
